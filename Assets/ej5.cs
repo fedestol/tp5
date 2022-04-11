5fdc217fe08f3a0d33d9b31d1b2dd65d983fbc91
fedestol <101721014+fedestol@users.noreply.github.com>
2022-04-11 08:35:51 -0300

    tp5 completed

--- Assets/ej5.cs
+++ Assets/ej5.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class ej5 : MonoBehaviour
 {
+
+    public int num1;
+    public int num2;
+    public int res;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+       res=  num1/num2 ;
+       
+
+        if ( num2 == 0 )
+            {
+
+            Debug.Log("no se puede dividir por cero");
+
+        }
+
+        else
+
+            Debug.Log(num1 + "/" + num2 + "=" + res);
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
